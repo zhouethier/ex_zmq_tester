a8ddcf1b241fe4d91be06f070291cdee0fd18302
John Warwick <jwarwick@gmail.com>
2014-10-24 12:03:42 -0400

    getting zmq working

--- lib/zmq_subscriber.ex
+++ lib/zmq_subscriber.ex
@@ -1,46 +1,58 @@
-require Logger
-
 defmodule ZmqSubscriber do
-	use GenServer
-	alias ZmqInterface.State
-	
-	@ip "10.1.3.109"
-	@sub_port "31285"
-	
-	# public API
-	def start_link do
-		GenServer.start_link(__MODULE__, [], [])
-	end
-	
-	# GenServer Interface
-	def init(_arg) do
-		{:ok, context} = :erlzmq.context()
-		{:ok, zmq_subscriber} = :erlzmq.socket(context, [:sub])
-		sub_addr = 'tcp://#{@ip}:#{@sub_port}'
-		:erlzmq.bind(zmq_subscriber, sub_addr)
-		:erlzmq.setsockopt(zmq_subscriber, :subscribe, <<>>)
-		
-		loop(zmq_subscriber)
-		
-		Logger.debug "starting zmq_subscriber, binding to #{inspect sub_addr}"
-		{:ok, %State{subscriber: zmq_subscriber}}
-	end
-	
-	def handle_info({:zmq_msg, data}, state) do
-		Logger.debug "ZmqSubscriber: handle incoming zmq_msg #{inspect data}"
-		{:noreply, state}
-	end
-	
-	def handle_info(msg, state) do
-		Logger.debug "ZmqSubscriber: handle incoming msg #{inspect msg}"
-		{:noreply, state}
-	end
-	
-	# private API
-	defp loop(sub) do
-		{ok, data} = :erlzmq.recv(sub)
-		Logger.debug "ZmqSubscriber, recv data #{inspect data}"
-		
-		loop(sub)
-	end
+  use GenServer
+  require Logger
+  # alias ZmqInterface.State
+
+  # @ip "127.0.0.1"
+  @sub_port "31285"
+
+  defmodule State do
+    defstruct socket: nil, data: []
+  end
+
+  # public API
+  def start_link(), do: start_link(%{})
+  def start_link(args) do
+    GenServer.start_link(__MODULE__, [args])
+  end
+
+  # GenServer Interface
+  def init([args]) do
+    port = Map.get(args, :port, @sub_port)
+    {:ok, context} = :erlzmq.context()
+    {:ok, zmq_subscriber} = :erlzmq.socket(context, [:sub, {:active, true}])
+    :ok = :erlzmq.setsockopt(zmq_subscriber, :subscribe, <<>>)
+    sub_addr = 'tcp://*:#{port}'
+    :ok = :erlzmq.bind(zmq_subscriber, sub_addr)
+
+    # loop(zmq_subscriber)
+
+    Logger.debug "starting zmq_subscriber, binding to #{inspect sub_addr}"
+    # {:ok, %State{subscriber: zmq_subscriber}}
+    {:ok, %State{socket: zmq_subscriber}}
+  end
+
+  def handle_info(data = {:zmq, _socket, msg, [:rcvmore | _rest]}, state) do
+    Logger.debug "ZmqSubscriber: rcvmore data: #{inspect data}"
+    {:noreply, %State{state | data: [msg | state.data]}}
+  end
+
+  def handle_info(data = {:zmq, _socket, msg, _options}, state) do
+    Logger.debug "ZmqSubscriber: handle incoming zmq_msg #{inspect data}"
+    Logger.debug "ZmqSubscriber: message: #{inspect Enum.reverse([msg | state.data])}"
+    {:noreply, %State{state | data: []}}
+  end
+
+  def handle_info(msg, state) do
+    Logger.debug "ZmqSubscriber: handle incoming msg #{inspect msg}"
+    {:noreply, state}
+  end
+
+  # private API
+  # defp loop(sub) do
+  #   {ok, data} = :erlzmq.recv(sub)
+  #   Logger.debug "ZmqSubscriber, recv data #{inspect data}"
+
+  #   loop(sub)
+  # end
 end
